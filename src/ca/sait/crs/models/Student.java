426552490a1708001331b085deb011fa3b2c6ad3
yvnmnl <yvana.gaddao@edu.sait.ca>
2026-02-04 00:39:58 -0700

    Make Student class immutable
    made student class immutable by declaring the class and its fields as final, initializing all values in the constructor, and removing setter methods. The toString() method now returns the student’s name directly.

--- src/ca/sait/crs/models/Student.java
+++ src/ca/sait/crs/models/Student.java
@@ -7,16 +7,16 @@ package ca.sait.crs.models;
  * @author Nick Hamnett <nick.hamnett@sait.ca>
  * @since June 1, 2023
  */
-public class Student implements ca.sait.crs.contracts.Student {
+public final class Student implements ca.sait.crs.contracts.Student {
     /**
      * Name of student
      */
-    private String name;
+    private final String name;
 
     /**
      * Students GPA (between 0.00 and 4.00)
      */
-    private double gpa;
+    private final double gpa;
 
     /**
      * Initializes instance.
@@ -24,8 +24,8 @@ public class Student implements ca.sait.crs.contracts.Student {
      * @param gpa Student's GPA
      */
     public Student(String name, double gpa) {
-        this.setName(name);
-        this.setGpa(gpa);
+        this.name = name;
+        this.gpa = gpa;
     }
 
     /**
@@ -45,27 +45,11 @@ public class Student implements ca.sait.crs.contracts.Student {
     }
 
     /**
-     * Sets the students name.
-     * @param name Name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Sets the students GPA
-     * @param gpa GPA
-     */
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
-    }
-
-    /**
      * Transforms Student instance to String
      * @return Student's name
      */
     @Override
     public String toString() {
-        return this.getName();
+        return name;
     }
 }
